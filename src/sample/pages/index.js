8c3363034ea1569dec29a99c22eec59e81a7e523
Sergio Rodrigues Pinto <sergio@devindex.com.br>
2020-03-13 12:09:47 -0300

    Disabling unfinished components

--- src/sample/pages/index.js
+++ src/sample/pages/index.js
@@ -1,44 +1,51 @@
 import Autocomplete from './Autocomplete';
 import Calendar from './Calendar';
-import Dropdown from './Dropdown';
+// import Dropdown from './Dropdown';
 import Modal from './Modal';
-import Notification from './Notification';
+// import Notification from './Notification';
 import Pagination from './Pagination';
 import Tabs from './Tabs';
-import Toast from './Toast';
+// import Toast from './Toast';
 
 export default [
   {
     id: 'autocomplete',
     name: 'Autocomplete',
     component: Autocomplete,
-  }, {
+  },
+  {
     id: 'calendar',
     name: 'Calendar',
     component: Calendar,
-  }, {
-    id: 'dropdown',
-    name: 'Dropdown',
-    component: Dropdown,
-  }, {
+  },
+  // {
+  //   id: 'dropdown',
+  //   name: 'Dropdown',
+  //   component: Dropdown,
+  // },
+  {
     id: 'modal',
     name: 'Modal',
     component: Modal,
-  }, {
-    id: 'notification',
-    name: 'Notification',
-    component: Notification,
-  }, {
+  },
+  // {
+  //   id: 'notification',
+  //   name: 'Notification',
+  //   component: Notification,
+  // },
+  {
     id: 'pagination',
     name: 'Pagination',
     component: Pagination,
-  }, {
+  },
+  {
     id: 'tabs',
     name: 'Tabs',
     component: Tabs,
-  }, {
-    id: 'toast',
-    name: 'Toast',
-    component: Toast,
-  }
+  },
+  // {
+  //   id: 'toast',
+  //   name: 'Toast',
+  //   component: Toast,
+  // },
 ];
