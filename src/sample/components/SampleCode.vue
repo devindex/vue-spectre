a3216b47254aa58cedd2a7a877c40e6a64d2197d
Sergio Rodrigues Pinto <sergio@devindex.com.br>
2021-07-27 12:02:38 -0300

    [vue-3] Created `<sample-code>` component for internal use

---
+++ src/sample/components/SampleCode.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="sample-code">
+    <div class="sample-code-title">Sample code</div>
+    <div class="sample-code-content">
+      <pre v-highlightjs="code"><code :class="lang"></code></pre>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    code: {
+      type: String,
+    },
+    lang: {
+      type: String,
+      default: 'html'
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+@import '~spectre.css/src/variables';
+
+.sample-code {
+  border: $border-width solid $border-color;
+  border-radius: $border-radius;
+  margin-top: $unit-10;
+
+  .sample-code-title {
+    border-bottom: $border-width solid $border-color;
+    color: $gray-color-dark;
+    padding: $layout-spacing $unit-5;
+  }
+
+  .sample-code-content {
+    code {
+      line-height: unset;
+      padding: $unit-5;
+    }
+    pre {
+      margin: 0;
+    }
+  }
+}
+</style>
--- src/sample/main.js
+++ src/sample/main.js
@@ -6,10 +6,13 @@ import 'spectre.css/dist/spectre-exp.css';
 import 'spectre.css/dist/spectre-icons.css';
 import 'highlight.js/styles/atom-one-light.css';
 import App from './App.vue';
+import SampleCode from '@/sample/components/SampleCode.vue';
 
 const app = createApp(App);
 
 app.use(VueSpectre, { locale: 'en' });
 app.use(VueHighlight);
+
+app.component('sample-code', SampleCode);
 
 app.mount('#app');
