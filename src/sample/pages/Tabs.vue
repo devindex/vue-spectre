e4e21e3b6bbf12c40d26f28cb634dc1af30d9380
Sergio Rodrigues Pinto <sergio@devindex.com.br>
2021-08-04 17:21:42 -0300

    [vue-3] Added `action` slot in `dx-tabs` component

--- src/sample/pages/Tabs.vue
+++ src/sample/pages/Tabs.vue
@@ -1,19 +1,45 @@
 <template>
   <h3 class="s-title">Tabs</h3>
 
-  <dx-tabs @change="handler">
-    <dx-tab label="Tab 1">
-      <p>Tab content 1</p>
-    </dx-tab>
-    <dx-tab label="Tab 2">
-      <p>Tab content 2</p>
-    </dx-tab>
-    <dx-tab label="Tab 3" :disabled="true">
-      <p>Tab content 3</p>
-    </dx-tab>
-  </dx-tabs>
+  <section class="component-section">
+    <h4 class="section-title">Basic</h4>
 
-  <sample-code :code="code"></sample-code>
+    <dx-tabs @change="handler">
+      <dx-tab label="Tab 1">
+        <p>Tab content 1</p>
+      </dx-tab>
+      <dx-tab label="Tab 2">
+        <p>Tab content 2</p>
+      </dx-tab>
+      <dx-tab label="Tab 3" :disabled="true">
+        <p>Tab content 3</p>
+      </dx-tab>
+    </dx-tabs>
+
+    <sample-code :code="code1"></sample-code>
+  </section>
+
+  <section class="component-section">
+    <h4 class="section-title">Advanced</h4>
+
+    <dx-tabs @change="handler">
+      <template #action>
+        <div class="input-group input-inline">
+          <input class="form-input input-sm" type="text">
+          <button class="btn btn-primary btn-sm input-group-btn">Search</button>
+        </div>
+      </template>
+
+      <dx-tab label="Tab Label 1">
+        <p>Tab content 1</p>
+      </dx-tab>
+      <dx-tab label="Tab Label 2">
+        <p>Tab content 2</p>
+      </dx-tab>
+    </dx-tabs>
+
+    <sample-code :code="code2"></sample-code>
+  </section>
 </template>
 
 <script>
@@ -29,10 +55,27 @@ const code1 = `<dx-tabs @change="handler">
   </dx-tab>
 </dx-tabs>`;
 
+const code2 = `<dx-tabs @change="handler">
+  <template v-slot:action>
+    <div class="input-group input-inline">
+      <input class="form-input input-sm" type="text">
+      <button class="btn btn-primary btn-sm input-group-btn">Search</button>
+    </div>
+  </template>
+
+  <dx-tab label="Tab Label 1">
+    <p>Tab content 1</p>
+  </dx-tab>
+  <dx-tab label="Tab Label 2">
+    <p>Tab content 2</p>
+  </dx-tab>
+</dx-tabs>`;
+
 export default {
   data() {
     return {
-      code: code1,
+      code1,
+      code2,
     };
   },
   methods: {
